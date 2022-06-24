326bc16bcab8939eed38444a511a17032c082c7a
igordonin <igordonin@gmail.com>
2022-06-24 12:04:16 -0300

    chore: add prisma init and schema

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,10 @@
 import { ApolloServer, gql } from 'apollo-server';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+
+const envConfig = dotenv.config();
+dotenvExpand.expand(envConfig);
+console.log({ DB_URL: process.env['DB_URL'] });
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
