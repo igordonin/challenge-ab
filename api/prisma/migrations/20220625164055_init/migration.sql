4db0963889c7ace2a7a7e1037353767de11ed628
igordonin <igordonin@gmail.com>
2022-06-25 14:20:28 -0300

    chore: fix database schema and recreate initial migration

--- api/prisma/migrations/20220625164055_init/migration.sql
+++ api/prisma/migrations/20220625164055_init/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
 CREATE TABLE "Transaction" (
-    "id" TEXT NOT NULL,
-    "accountId" TEXT NOT NULL,
-    "categoryId" TEXT NOT NULL,
+    "id" UUID NOT NULL,
+    "accountId" UUID NOT NULL,
+    "categoryId" UUID,
     "reference" TEXT,
     "amount" DOUBLE PRECISION NOT NULL,
     "currency" CHAR(3) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE "Transaction" (
 
 -- CreateTable
 CREATE TABLE "Category" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
     "name" TEXT NOT NULL,
     "color" CHAR(6),
 
@@ -22,14 +22,14 @@ CREATE TABLE "Category" (
 
 -- CreateTable
 CREATE TABLE "Account" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
     "name" TEXT NOT NULL,
 
     CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
